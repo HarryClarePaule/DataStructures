8a715e42b7267ca145565c9b4df5c2bdad866f33
Harry Clare-Paule <harry.clare.paule@nanoavionics.com>
2023-03-01 16:16:32 +0000

    some code commented out

--- hashTable.py
+++ hashTable.py
@@ -53,28 +53,28 @@ class HashTable:
                 del self.arr[h][index]
 
 
-h = HashTable()
+# h = HashTable()
 
-print(get_hash('march'))
-print(get_hash('archm'))
-print('Break')
-h['june 7'] = 207 # uses the '__setitem__' function python operator
-h['march 10'] = 400
-h['sept 20'] = 500
-h['aug 21'] = 321
-h['march 6'] = 321
-h['archm 6'] = 439
+# print(get_hash('march'))
+# print(get_hash('archm'))
+# print('Break')
+# h['june 7'] = 207 # uses the '__setitem__' function python operator
+# h['march 10'] = 400
+# h['sept 20'] = 500
+# h['aug 21'] = 321
+# h['march 6'] = 321
+# h['archm 6'] = 439
 
-print(h.arr)
-print('Break')
-print(h['june 7']) # usees the '__getitem' function python operator
-print('Break')
+# print(h.arr)
+# print('Break')
+# print(h['june 7']) # usees the '__getitem' function python operator
+# print('Break')
 
-print(h.arr)
-print(h['archm 6'])
-print(h['march 6'])
-del h['march 6']
-print(h.arr)
+# print(h.arr)
+# print(h['archm 6'])
+# print(h['march 6'])
+# del h['march 6']
+# print(h.arr)
 
 
 
